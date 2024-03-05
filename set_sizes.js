30c713dedea2d48e24c2543a6409ea0b16805ee1
RomanovskiiArsenii <romanovskyway@gmail.com>
2024-03-06 00:45:17 +0400

    changed logic

---
+++ set_sizes.js
@@ -0,0 +1,42 @@
+const setSizes = (() => {
+    const container = document.getElementById('images_container');
+    const image = document.getElementById('first_image');
+    const sizes = { small: '40vw', large: '80vw' };
+    let rootStyles;
+
+    const setContainerHeight = () => {
+        container.style.height = `${image.offsetHeight}px`;
+    };
+
+    const setScreenCoeff = () => {
+        document.documentElement.style.setProperty('--coeff', sizes.small);
+        rootStyles = getComputedStyle(document.documentElement);
+    };
+
+    const changeScreenCoeff = () => {
+        const screenCoeff = rootStyles.getPropertyValue('--coeff').trim();
+
+        if (screenCoeff === sizes.small) {
+            document.documentElement.style.setProperty('--coeff', sizes.large);
+            setContainerHeight();
+            console.log('coeff = 80vw');
+        } else {
+            document.documentElement.style.setProperty('--coeff', sizes.small);
+            setContainerHeight();
+            console.log('coeff = 40vw');
+        }
+    };
+
+    return {
+        setContainerHeight: setContainerHeight,
+        setScreenCoeff: setScreenCoeff,
+        changeScreenCoeff: changeScreenCoeff,
+    };
+})();
+
+const container = document.getElementById('images_container');
+
+window.addEventListener('load', setSizes.setScreenCoeff);
+window.addEventListener('load', setSizes.setContainerHeight);
+window.addEventListener('resize', setSizes.setContainerHeight);
+window.addEventListener('click', setSizes.changeScreenCoeff);
